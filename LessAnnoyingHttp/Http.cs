c40d5929309f448167ee86c27478cb96984fbc42
DemonExposer <kbdmns@gmail.com>
2026-03-31 17:56:59 +0300

    Added async methods

--- LessAnnoyingHttp/Http.cs
+++ LessAnnoyingHttp/Http.cs
@@ -15,7 +15,7 @@ public static class Http {
 	/// <param name="endpoint">The URL to send the request to</param>
 	/// <param name="headers">Optional headers</param>
 	/// <returns><see cref="Response"/></returns>
-	public static Response Get(string endpoint, Header[]? headers = null) {
+	public static async Task<Response> GetAsync(string endpoint, Header[]? headers = null) {
 		using HttpClient client = new ();
 		client.Timeout = TimeSpan.FromSeconds(Timeout);
 		HttpRequestMessage request = new () {
@@ -29,7 +29,7 @@ public static class Http {
 		
 		HttpResponseMessage response;
 		try {
-			response = client.SendAsync(request).Result;
+			response = await client.SendAsync(request);
 		} catch (TaskCanceledException) {
 			throw new TimeoutException($"Timeout waiting for response for request to {endpoint}");
 		} catch (Exception e) {
@@ -38,8 +38,10 @@ public static class Http {
 		
 		return new Response { IsSuccessful = response.IsSuccessStatusCode, Body = response.Content.ReadAsStringAsync().Result, Headers = new Dictionary<string, IEnumerable<string>>(response.Headers.ToDictionary(), StringComparer.OrdinalIgnoreCase), StatusCode = response.StatusCode };
 	}
+	
+	public static Response Get(string endpoint, Header[]? headers = null) => GetAsync(endpoint, headers).GetAwaiter().GetResult();
 
-	private static Response BodyRequest(string endpoint, HttpMethod method, string body, Header[]? headers, string contentType) {
+	private static async Task<Response> BodyRequest(string endpoint, HttpMethod method, string body, Header[]? headers, string contentType) {
 		using HttpClient client = new ();
 		client.Timeout = TimeSpan.FromSeconds(Timeout);
 		HttpRequestMessage request = new () {
@@ -54,7 +56,7 @@ public static class Http {
 		
 		HttpResponseMessage response;
 		try {
-			response = client.SendAsync(request).Result;
+			response = await client.SendAsync(request);
 		} catch (TaskCanceledException) {
 			throw new TimeoutException($"Timeout waiting for response for request to {endpoint}");
 		} catch (Exception e) {
@@ -72,7 +74,9 @@ public static class Http {
 	/// <param name="headers">Optional headers</param>
 	/// <param name="contentType">The content type of the body - default = application/json</param>
 	/// <returns><see cref="Response"/></returns>
-	public static Response Delete(string endpoint, string body, Header[]? headers = null, string contentType = "application/json") => BodyRequest(endpoint, HttpMethod.Delete, body, headers, contentType);
+	public static async Task<Response> DeleteAsync(string endpoint, string body, Header[]? headers = null, string contentType = "application/json") => await BodyRequest(endpoint, HttpMethod.Delete, body, headers, contentType);
+	
+	public static Response Delete(string endpoint, string body, Header[]? headers = null, string contentType = "application/json") => DeleteAsync(endpoint, body, headers, contentType).GetAwaiter().GetResult();
 
 	/// <summary>
 	/// Sends a PATCH request
@@ -82,7 +86,9 @@ public static class Http {
 	/// <param name="headers">Optional headers</param>
 	/// <param name="contentType">The content type of the body - default = application/json</param>
 	/// <returns><see cref="Response"/></returns>
-	public static Response Patch(string endpoint, string body, Header[]? headers = null, string contentType = "application/json") => BodyRequest(endpoint, HttpMethod.Patch, body, headers, contentType);
+	public static async Task<Response> PatchAsync(string endpoint, string body, Header[]? headers = null, string contentType = "application/json") => await BodyRequest(endpoint, HttpMethod.Patch, body, headers, contentType);
+	
+	public static Response Patch(string endpoint, string body, Header[]? headers = null, string contentType = "application/json") => PatchAsync(endpoint, body, headers, contentType).GetAwaiter().GetResult();
 	
 	/// <summary>
 	/// Sends a POST request
@@ -92,7 +98,9 @@ public static class Http {
 	/// <param name="headers">Optional headers</param>
 	/// <param name="contentType">The content type of the body - default = application/json</param>
 	/// <returns><see cref="Response"/></returns>
-	public static Response Post(string endpoint, string body, Header[]? headers = null, string contentType = "application/json") => BodyRequest(endpoint, HttpMethod.Post, body, headers, contentType);
+	public static async Task<Response> PostAsync(string endpoint, string body, Header[]? headers = null, string contentType = "application/json") => await BodyRequest(endpoint, HttpMethod.Post, body, headers, contentType);
+	
+	public static Response Post(string endpoint, string body, Header[]? headers = null, string contentType = "application/json") => PostAsync(endpoint, body, headers, contentType).GetAwaiter().GetResult();
 	
 	/// <summary>
 	/// Sends a PUT request
@@ -102,5 +110,7 @@ public static class Http {
 	/// <param name="headers">Optional headers</param>
 	/// <param name="contentType">The content type of the body - default = application/json</param>
 	/// <returns><see cref="Response"/></returns>
-	public static Response Put(string endpoint, string body, Header[]? headers = null, string contentType = "application/json") => BodyRequest(endpoint, HttpMethod.Put, body, headers, contentType);
+	public static async Task<Response> PutAsync(string endpoint, string body, Header[]? headers = null, string contentType = "application/json") => await BodyRequest(endpoint, HttpMethod.Put, body, headers, contentType);
+	
+	public static Response Put(string endpoint, string body, Header[]? headers = null, string contentType = "application/json") => PutAsync(endpoint, body, headers, contentType).GetAwaiter().GetResult();
 }
